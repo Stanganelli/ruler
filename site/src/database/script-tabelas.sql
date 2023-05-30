1ae458001d93522e8d21475f983eb0145219d174
Enzo Victor Stanganelli Gama <stanganelli899@gmail.com>
2023-05-30 15:23:49 -0300

    Update script-tabelas.sql

--- site/src/database/script-tabelas.sql
+++ site/src/database/script-tabelas.sql
@@ -1,14 +1,24 @@
+create database ruler;
+use ruler;
 
-
-CREATE DATABASE ruler;
-
-USE ruler;
-
-CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50)
+create table usuario (
+idUsuario int primary key auto_increment,
+nome varchar(45),
+email varchar(45),
+senha varchar(45)
 );
 
+select * from usuario;
+select * from lider;
 
+create table lider (
+idLider int primary key auto_increment,
+nome varchar(45),
+fkUsuario int,
+constraint fkUsuario foreign key (fkUsuario)
+references usuario(idUsuario));
+ INSERT INTO lider (nome, fkUsuario) VALUES ("otoman", undefined);
+ 
+ 
+ INSERT INTO lider (nome, fkUsuario) VALUES ("greek", undefined)
+ 
