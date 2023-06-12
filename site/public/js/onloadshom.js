6fec7ca53d4331cee861f78ffaf358ca0500882d
Enzo Victor Stanganelli Gama <stanganelli899@gmail.com>
2023-06-12 19:10:37 -0300

    funções da home espandida

---
+++ site/public/js/onloadshom.js
@@ -0,0 +1,80 @@
+
+var img = localStorage.getItem("img")
+    var som = localStorage.getItem("som")
+
+    img1.innerHTML = img
+    som1.innerHTML = som
+
+
+
+
+    var ruler = localStorage.getItem("emperador")
+    var man = Number(localStorage.getItem("manp"))
+
+    var art = Number(localStorage.getItem("arti"))
+
+    var cavalary = Number(localStorage.getItem("cav"))
+
+    var csh = Number(localStorage.getItem("cash"))
+
+    var moral = Number(localStorage.getItem("moral"))
+
+    var forcaart = Number(localStorage.getItem("forcaart"))
+
+    var forcacav = Number(localStorage.getItem("forcacav"))
+
+    var forcaman = Number(localStorage.getItem("forcaman"))
+
+    var forcabal = Number(localStorage.getItem("forcabal"))
+divexib.innerHTML = `No momento você tem:<br>${csh} ${moneytim}<br> ${man} soldados de infantria,<br> ${cavalary}de horseman.<br>${art} linhas especializadas`
+
+
+
+    if (ruler == "roman") {
+        moneytim = "denarius"
+
+    }
+    if (ruler == "greek") {
+        moneytim = "drachmas"
+    }
+    if (ruler == "franc") {
+
+        moneytim = "centimes"
+    }
+    if (ruler == "otoman") {
+        moneytim = "liras"
+
+    }
+    if (ruler == "perisan") {
+        moneytim = "dìngs"
+    }
+
+    divexib.innerHTML = `No momento você tem:<br>${csh} ${moneytim}<br> ${man} soldados de infantria,<br> ${cavalary}de horseman.<br>${art} linhas especializadas`
+
+
+    if (ruler == "roman") {
+        moneytim = "denarius"
+
+        spanart.innerHTML = "linha de arqueiros"
+    }
+    if (ruler == "greek") {
+        moneytim = "drachmas"
+        spanart.innerHTML = "linha de arqueiros"
+    }
+    if (ruler == "franc") {
+
+        spanart.innerHTML = "canhão"
+        moneytim = "centimes"
+
+        spanart.innerHTML = "armadas de canhões"
+    }
+    if (ruler == "otoman") {
+        moneytim = "liras"
+
+        spanart.innerHTML = "armadas de canhões"
+    }
+    if (ruler == "perisan") {
+        moneytim = "dìngs"
+
+        spanart.innerHTML = "linha de arqueiros"
+    }
