e786d29a2f562fba4f998531cde7b895f2ec0843
Enzo Victor Stanganelli Gama <stanganelli899@gmail.com>
2023-06-17 15:15:10 -0300

    tabela com os funcionamento

--- site/src/database/script-tabelas.sql
+++ site/src/database/script-tabelas.sql
@@ -1,3 +1,5 @@
+
+drop database ruler;
 create database ruler;
 use ruler;
 
@@ -5,20 +7,36 @@ create table usuario (
 idUsuario int primary key auto_increment,
 nome varchar(45),
 email varchar(45),
-senha varchar(45)
-);
+senha varchar(45),
+fkEmperador int,
+constraint fkEmperador foreign key (fkEmperador)
+references lider(idLider));
 
 select * from usuario;
 select * from lider;
 
+insert into lider(nome) value
+('napoleao'),
+('alexandre'),
+('quin'),
+('mehemed2'),
+('augusto');
+
 create table lider (
 idLider int primary key auto_increment,
-nome varchar(45),
-fkUsuario int,
-constraint fkUsuario foreign key (fkUsuario)
-references usuario(idUsuario));
+nome varchar(45));
+insert into usuario (nome,fkEmperador) value
+('sim', 1),
+('a', 2),
+('n', 1),
+('iga', 2);
+
+
+
  INSERT INTO lider (nome, fkUsuario) VALUES ("otoman", undefined);
  
  
- INSERT INTO lider (nome, fkUsuario) VALUES ("greek", undefined)
+ INSERT INTO lider (nome, fkUsuario) VALUES ("greek", undefined);
  
+ 
+ update  usuario set fkemperador = 1 where idUsuario = 2;
