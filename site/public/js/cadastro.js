068cf36a670d4af460d72468e3d9e89ff527eb31
Enzo Victor Stanganelli Gama <stanganelli899@gmail.com>
2023-09-02 14:52:04 -0300

    feat ajustando e comentando as funcionalidades de cadastro e login
    Nesse commit "arrumei" o cadastro e o login, além de comentar

---
+++ site/public/js/cadastro.js
@@ -0,0 +1,69 @@
+//aqui tiro o cadastro do html e faço uma pasta dele para melhor entendimento
+function cadastrar() {
+//crio as variaveis de acordo com os inputs do usuario
+    var nomeVar = nome_input.value;
+    var emailVar = email_input.value;
+    var senhaVar = senha_input.value;
+    var confirmacaoSenhaVar = confirmacao_senha_input.value;
+//seto as divs de erro como vazias
+    erro_email.innerHTML = "";
+    erro_senha.innerHTML = "";
+    erro_confirmar_senha.innerHTML = "";
+
+    //verifico se algum dos campos estão vazios
+    if (nomeVar == "" || emailVar == "" || senhaVar == "" || confirmacaoSenhaVar == "") {
+        alert("Preencha os campos vazios")
+
+    } else {
+        //se nenhum estivesse, aqui verifico se a emailVar não tem @ e se não termina com .com
+if (emailVar.indexOf("@") == -1 || emailVar.endsWith(".com") == false) {
+//se atender as condições acima exibo
+            erro_email.innerHTML = "email inválido"
+            input_email.className = "invalido"
+
+//se faltar @
+            if (emailVar.indexOf("@") == -1) {
+
+                erro_email.innerHTML = `Está faltando o @`
+
+//se não terminar com .com
+            }
+            else if (emailVar.endsWith(".com") == false) {
+
+                erro_email.innerHTML = `Está faltando o ".com"`
+
+            }
+
+        }
+//se for menor que 6 caracteres
+        else if (senhaVar.length < 8) {
+            erro_senha.innerHTML = 'Senha inválida!'
+            erro_senha.innerHTML += ' Precisa ter ao menos 8 dígitos'
+            input_senha.className = 'invalido'
+
+        }
+
+        else if
+        //se for diferente
+            (confirmacaoSenhaVar != senhaVar) {
+            erro_confirmar_senha.innerHTML = `A senha de confirmação não corresponde à senha inserida. Por favor, tente novamente`
+            input_confirmar_senha.className = "invalido"
+
+        }
+        window.location = 'index.html'
+    }
+//chamo na usuarios.js o caminho cadastras
+    fetch("/usuarios/cadastrar", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            //decido oq vou "stringifar"
+            nomeServer: nomeVar,
+            emailServer: emailVar,
+            senhaServer: senhaVar,
+        })
+    })
+
+}
--- site/src/models/usuarioModel.js
+++ site/src/models/usuarioModel.js
@@ -32,7 +32,7 @@ function cadastrar(nome, email, senha) {
 }
 
 
-
+//mando aqui no bd
 function enviarRuler(lider, idUsuario) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", lider);
 
